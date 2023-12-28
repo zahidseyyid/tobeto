73cfe03f8c88d1db3aed3151ca47375e4d1ae603
Hamza Zahid <zahidseyyid@gmail.com>
2023-12-28 15:57:52 +0300

    delete macos build

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/cheavbella/projectFlutter/blog_project/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/cheavbella/projectFlutter/blog_project/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
